d5dbd180b364fed14df7d65810bd9588f9bef7cf
Lucas B <ajedrezalmafuerte@gmail.com>
2023-05-08 13:31:18 -0300

    Probamos a solucionar un error de la propiedad file

--- routes/files.router.js
+++ routes/files.router.js
@@ -72,7 +72,7 @@ router.post('/upload',
   upload.single("file"),
   async (req, res, next) => {
     res.send({ data: req.file, msg: "Exito" });
-    console.log(res.send({req1: req.files, req2: req.files.file}));
+    // console.log(res.send({req1: req.files, req2: req.files.file}));
 
     // try {
     //   const fileUpload = await service.uploadFile(req.files.file)
