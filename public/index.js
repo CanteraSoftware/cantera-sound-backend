d62161f1bd8566b51f930a3ff049fb70f1da18af
Lucas B <ajedrezalmafuerte@gmail.com>
2023-05-10 17:08:16 -0300

    Ligeros cambios

--- public/index.js
+++ public/index.js
@@ -3,8 +3,12 @@ const url = 'http://18.117.98.49:2000/api/v1/files'
 
 
 const getAPI = async () => {
-  const response = await fetch(url)
+  try {
+    const response = await fetch(url)
   const data = await response.json()
   console.log(data)
+  } catch (err) {
+    console.error(`$Error: ${err}`)
+  }
 }
 getAPI()
