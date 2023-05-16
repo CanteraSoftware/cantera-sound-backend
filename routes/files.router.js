910a949e6c7482a19f98a723b4dceb9e7eb65b4a
Lucas B <ajedrezalmafuerte@gmail.com>
2023-05-16 12:02:22 -0300

    Optimizamos el código

--- routes/files.router.js
+++ routes/files.router.js
@@ -58,6 +58,44 @@ router.post('/upload',
   async (req, res, next) => {
 
     try {
+      const imageFormat = [
+        "jpeg",
+        "png"
+      ]
+
+      const musicFormat = [
+        "mpeg",
+        "mp4",
+        "aac",
+        "ogg"
+      ]
+
+      const commaSeparator = format => `${Object.keys(format).slice(0, format.length-2).join(', ')} or ${format.length-1}`
+
+      const errorStatus = {
+          conflict: "Conflict",
+          notAcceptable: "Not Acceptable",
+          notFound: "Not Found"
+      }
+
+      const errorMessage = {
+        conflictRows: "Conflict with same name rows",
+        urlTooLong: "Url longer than expected",
+        fileTooBig: "File is very big. Send a smaller image or compress the file",
+        invalidImageType: `This file must be of type ${commaSeparator(imageFormat)}`,
+        invalidVideoType: `This file must be of type ${commaSeparator(musicFormat)}`,
+        unknownFileType: "Unknown file type. Send a file of a different type",
+        notFound: "File not found"
+      }
+
+      const returnError = (statusCode, error, message) => {
+        return res.status(statusCode).json({
+          statusCode,
+          error,
+          message
+        });
+      }
+
       // Require body of the user
       const body = req.body;
       // Select name and url of file and find if this name be repite
@@ -65,82 +103,45 @@ router.post('/upload',
       // Find any same values
       const validatorConcidences = (rowFilter, nameFilter, key) => rowFilter.some(row => row[key] === nameFilter);
       if (validatorConcidences(results.rows, body.nameFile, "nameFile") || validatorConcidences(results.rows, body.fileUrl, "fileUrl")) {
-        return res.status(409).json({
-          "statusCode": 409,
-          "error": "Conflict",
-          "message": "Conflict with same name rows"
-        });
+        return returnError(409, errorStatus.conflict, errorMessage.conflictRows);
       }
 
       const files = JSON.parse(JSON.stringify(req.files));
       const { name, size, mimetype } = files.file || files.fileUrl;
       const url = `https://${config.bucketName}.s3.${config.bucketRegion}.amazonaws.com/${name}`;
 
+      const sizeLimit = 9000;
+
       const fileFilter = file => {
-        const imageFormat = {
-          jpeg: "jpeg",
-          png: "png"
-        }
-
-        const musicFormat = {
-          mpeg: "mpeg",
-          mp4: "mp4",
-          aac: "aac",
-          ogg: "ogg"
-      }
-
         const format = file.split("/");
 
-        return format[0] === "image" ? { "image": imageFormat[format[1]] } :
-              format[0] === "video" ? { "video": musicFormat[format[1]] } :
-              { "unknown": format[0] };
+        return format[0] === "image" ? { image: imageFormat.includes(format[1]) } :
+          format[0] === "video" ? { video: musicFormat.includes(format[1]) } :
+            { unknown: true };
       }
 
       if (url.length > 95) {
-        return res.status(404).json({
-          "statusCode": 406,
-          "error": "Not Acceptable",
-          "message": "Url longer than expected"
-        });
+        return returnError(406, errorStatus.notAcceptable, errorMessage.urlTooLong);
       }
 
-      if ((size / 1024).toFixed(2) > 9000) {
-        return res.status(404).json({
-          "statusCode": 406,
-          "error": "Not Acceptable",
-          "message": "File is very big. Send a image more smoll or compress the file"
-        });
+      if ((size / 1024).toFixed(2) > sizeLimit) {
+        return returnError(406, errorStatus.notAcceptable, errorMessage.fileTooBig);
       }
 
-      if (fileFilter(mimetype).image !== undefined && fileFilter(mimetype).image) {
-        return res.status(404).json({
-          "statusCode": 406,
-          "error": "Not Acceptable",
-          "message": "This file must be of type jpeg or png"
-        });
-      } else if (fileFilter(mimetype).video !== undefined && fileFilter(mimetype).video) {
-        return res.status(404).json({
-          "statusCode": 406,
-          "error": "Not Acceptable",
-          "message": "This file must be of type mp3, mp4, acc or ogg"
-        });
-      } else if (fileFilter(mimetype).unknown) {
-        return res.status(404).json({
-          "statusCode": 406,
-          "error": "Not Acceptable",
-          "message": `${fileFilter(mimetype).unknown} is unknown file type. Send a file of a different type`
-        });
+      const filterResult = fileFilter(mimetype);
+      if (filterResult.image !== undefined && filterResult.image) {
+        return returnError(406, errorStatus.notAcceptable, errorMessage.invalidImageType);
+      } else if (filterResult.video !== undefined && filterResult.video) {
+        return returnError(406, errorStatus.notAcceptable, errorMessage.video);
+      } else if (filterResult.unknown) {
+        return returnError(406, errorStatus.notAcceptable, errorMessage.unknownFileType);
       }
 
       const downloadFile = async (fileName) => {
         try {
           const file = await service.getFile(fileName);
           if (!file) {
-            return res.status(404).json({
-              "statusCode": 404,
-              "error": "Not Found",
-              "message": "File not found"
-            });
+            return returnError(404, errorStatus.notFound, errorMessage.notFound);
           }
           const fileUrl = await service.downloadFile(fileName);
           return fileUrl
